2ddc616899e6618a5a3ac75265f3966d3ce904ff
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2024-10-11 18:33:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env bash
 
+main() {
+    local path_align
+    if [[ -d /data/data/com.termux/files/usr ]]; then
+        path_align="/data/data/com.termux/files/usr/bin"
+    elif [[ -d /usr/bin ]]; then
+        path_align="/usr/bin"
+    else
+        echo "Error: Required directory not found."
+        exit 1
+    fi
+    if [[ $(id -u) -ne 0 ]]; then
+        echo "Error : This script must be run as root."
+        exit 1
+    fi 
+}
+
+main
