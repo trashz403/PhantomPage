1dae447a0fdd362082e3b63807bfc4ed2e8771e6
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2024-10-11 18:42:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,9 +10,20 @@ main() {
         echo "Error: Required directory not found."
         exit 1
     fi
-    if [[ $(id -u) -ne 0 ]]; then
-        echo "Error : This script must be run as root."
-        exit 1
+
+    if [[ -d /usr/bin ]]; then
+        if [[ $(id -u) -ne 0 ]]; then
+            echo "Error: This script must be run as root."
+            exit 1
+        fi
+    fi
+
+    mv bin/phantom "$path_align"
+    
+    if [[ -f "$path_align/phantom" ]]; then
+        chmod +x "$path_align/phantom"
+    else
+        echo "You can't run this tool like a command line. Please try again."
     fi 
 }
 
